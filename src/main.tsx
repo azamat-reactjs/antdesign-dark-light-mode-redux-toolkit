b48b68190317e051f799b238c1e5e040ddf341d2
azamatcodes <azamatcodes@gmail.com>
2023-02-21 23:18:08 +0500

    Add Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import 'antd/dist/reset.css'
 import './styles/index.scss'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
